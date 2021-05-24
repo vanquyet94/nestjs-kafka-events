90076b9fb85896f2c54cd97520fd1c4872ceafc4
Max <mail@maxg-dev.org>
2021-05-24 20:46:42 +0200

    Add retry mechanism

---
+++ src/helpers/retry.util.ts
@@ -0,0 +1,33 @@
+const waitFor = <T>(seconds: number): Promise<T> =>
+  new Promise((r) => setTimeout(r, seconds * 1000));
+
+/**
+ * Retries Promises
+ * @param operation
+ * @param delay
+ * @param retries
+ * @param onError
+ */
+export const retry = <T>(
+  operation: () => Promise<T>,
+  delay = 2,
+  retries = 3,
+  onError?: (reason: any, retriesLeft: number) => void,
+) => {
+  return new Promise<T>((resolve, reject) => {
+    return operation()
+      .then(resolve)
+      .catch((reason) => {
+        if (onError) {
+          onError(reason, retries);
+        }
+        if (retries > 0) {
+          return waitFor<T>(delay)
+            .then(retry.bind(null, operation, delay, retries - 1))
+            .then(resolve)
+            .catch(reject);
+        }
+        return reject(reason);
+      });
+  });
+};
